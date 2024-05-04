c7be4732dbef7da7c8d6cf7063de15fc2aaef6dc
Jocelyn Turpault <turpault@gmail.com>
2024-05-04 13:26:46 -0400

    Disable Geo POI now that google requires payment (WTF ?)

--- server/background/bg-services.ts
+++ server/background/bg-services.ts
@@ -1,12 +1,12 @@
 import { buildFaceScan } from "./bg-faces";
 import { buildFavoriteFolder } from "./bg-favorites";
-import { buildGeolocation } from "./bg-geolocate";
+//import { buildGeolocation } from "./bg-geolocate";
 import { buildThumbs } from "./bg-thumbgen";
 
 export async function startBackgroundServices(exitOnComplete: boolean) {
   await Promise.all([
     buildThumbs(exitOnComplete),
-    buildGeolocation(exitOnComplete),
+    //buildGeolocation(exitOnComplete),
     buildFavoriteFolder(exitOnComplete),
     buildFaceScan(exitOnComplete),
   ]);
